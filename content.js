cec28f808829ce3e8b74cb0a8c927314112990f3
Drew Gallagher <drewg2009@gmail.com>
2016-07-31 16:38:13 -0400

    testing code

--- content.js
+++ content.js
@@ -9,9 +9,22 @@
  */
 
 $(document).ready(function () {
-    $(document).on("loadedmetadata", function () {
-        this.currentTime = 50;
-        console.log("Made it fucker");
+
+    jQuery(document).delegate('video', 'DOMNodeInserted', function () {
+
+        $('video').bind('play', function (e) {
+            alert('hey');
+            var video = $("video").eq(0)[0];
+            console.log(video);
+            console.log(video.currentTime);
+
+            video.currentTime=360;
+
+
+        });
+
     });
-    
+
+
+
 });
