26af8cc0b6fdf451f8f4fb9733935a99912929cf
Drew Gallagher <drewg2009@gmail.com>
2016-07-31 15:18:42 -0400

    added jQuery

--- content.js
+++ content.js
@@ -1,6 +1,13 @@
+/*
 window.onload = function () {
 	document.getElementsByTagName('video').addEventListener('loadedmetadata', function() {
   	this.currentTime = 50;
   	console.log("Made it fucker");
 }, false);
 }
+
+*/
+
+$(document).ready(function(){
+	alert('hey');
+});
