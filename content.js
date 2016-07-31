45ef4de91d8d4e11b0222d0a6fcce45ab3ac21e7
nick <capitals56@gmail.com>
2016-07-31 14:58:30 -0400

    Doesn't work.

--- content.js
+++ content.js
@@ -1,5 +1,6 @@
-// alert("hello");
-
-var x = document.getElementsByTagName("label");
-x[0].innerHTML = "8:00";
-    
+window.onload = function () {
+	document.getElementsByTagName('video').addEventListener('loadedmetadata', function() {
+  	this.currentTime = 50;
+  	console.log("Made it fucker");
+}, false);
+}
