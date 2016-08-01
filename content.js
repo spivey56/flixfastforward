be1ae3d6b0505f0fcb136c0b56c558785c4b967c
nick <capitals56@gmail.com>
2016-08-01 18:35:00 -0400

    Skips intro of Archer Season 2 episode 9.

--- content.js
+++ content.js
@@ -1,3 +1,4 @@
+/*
 $(document).ready(function () {
 
 
@@ -5,15 +6,7 @@ $(document).ready(function () {
 
         $('video').bind('play', function (e) {
 
-            if(!localStorage.getItem("skipped")) {
-
-                var skipTime = 1000;
-
-                window.open(window.location.href + "&t=" + skipTime, "_self");
-
-                localStorage.setItem("skipped", "FullHouse");
-
-            }
+            
 
 
         });
@@ -22,3 +15,26 @@ $(document).ready(function () {
 
 
 });
+*/
+	localStorage.clear();
+	time=setInterval(function(){
+		var time = $("label").text();
+		var tvLong = $("span").text();
+		var locNum = tvLong.search("Season");
+		var tvName = tvLong.slice(0, locNum);
+		var tvSeason = tvLong.slice(locNum+7, locNum+8);
+		var tvEpisode = tvLong.slice(locNum+14, locNum+15);	
+		
+		if(time==="19:18"){
+			if(!localStorage.getItem("skipped")) {
+                var skip = $("video").get(0).currentTime; 
+                var num = parseInt(skip);
+                num += 34;
+                window.open(window.location.href + "&t=" + num, "_self");
+                localStorage.setItem("skipped", tvName+"."+tvSeason+"."+tvEpisode);
+            }
+		}
+
+		console.log(time);
+
+	},500);
