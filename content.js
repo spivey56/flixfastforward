d4389a5c55b42b392b1bf90e1e593dd4c5db1087
Drew Gallagher <drewg2009@gmail.com>
2016-08-02 12:10:13 -0400

    separated functionality for video playing and first load

--- content.js
+++ content.js
@@ -1,69 +1,35 @@
-/*
- $(document).ready(function () {
+$(document).ready(function() {
+
+  var tvLong = $("span").text();
+  var locNum = tvLong.search("Season");
+  var tvName = tvLong.slice(0, locNum);
+  var tvSeason = tvLong.slice(locNum + 7, locNum + 8);
+  var tvEpisode = tvLong.slice(locNum + 14, locNum + 15);
+  var skipTime = 300;
+
+    if (!window.location.hash) {
+        //window.location = window.location + '#skipped';
+        window.open(window.location.href + "&t=" + skipTime + "#skipped", "_self");
+    }
+    else{
+      $("video").bind("play", function(e) {
+          time = setInterval(function() {
+
+              var skip = $("video").get(0).currentTime;
+              var time = parseInt(skip);
+              var timeSkip = 115; //Get this from database
+              var intro = 34; //call from database depending on tvName
+              var toThisPoint = timeSkip + intro;
+
+              if (time >= timeSkip && time < toThisPoint - 1) {
+                  window.open(window.location.href + "&t=" + toThisPoint + "#skipped", "_self");
+              }
 
 
- jQuery(document).delegate('video', 'DOMNodeInserted', function () {
+              console.log(time);
 
- $('video').bind('play', function (e) {
+          }, 100);
+      });
+    }
 
- if(!localStorage.getItem("skipped")) {
-
- var skipTime = 1000;
-
- window.open(window.location.href + "&t=" + skipTime, "_self");
-
- localStorage.setItem("skipped", "FullHouse");
-
-
-
- }
-
-
- });
-
- });
-
-
- });
- */
-$(document).ready(function () {
-    localStorage.clear();
-    time = setInterval(function () {
-
-        var tvLong = $("span").text();
-        var locNum = tvLong.search("Season");
-        var tvName = tvLong.slice(0, locNum);
-        var tvSeason = tvLong.slice(locNum + 7, locNum + 8);
-        var tvEpisode = tvLong.slice(locNum + 14, locNum + 15);
-        var skip = $("video").get(0).currentTime;
-        var time = parseInt(skip);
-        var timeSkip = 115;//Get this from database
-        var intro = 34; //call from database depending on tvName
-        var toThisPoint = timeSkip + intro;
-
-        if (time >= timeSkip && time < toThisPoint - 1) {
-            if (!localStorage.getItem("skipped")) {
-
-                $.ajax({
-                  url: window.location.href + "&t=" + toThisPoint,
-                  type: 'GET',
-                  success:function(){
-                    console.log("ran get request")
-                  },
-                  error:function(error){
-                    console.log(error);
-                  }
-
-                });
-
-                //window.open(window.location.href + "&t=" + toThisPoint, "_self");
-                localStorage.setItem("skipped", tvName + "." + tvSeason + "." + tvEpisode);
-                var id = '#dialog';
-
-            }
-        }
-
-        console.log(time);
-
-    }, 500);
 });
