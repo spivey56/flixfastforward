f11665be8247a6e3f00205de73bcdfc45ab2af54
nick <capitals56@gmail.com>
2016-08-03 16:57:37 -0400

    Knows when you're browsing vs watching.

--- content.js
+++ content.js
@@ -1,36 +1,28 @@
-
-/*
 $(document).ready(function () {
-time = setInterval(function(){}
+time = setInterval(function(){
   var currentWindow = window.location.href;
+  var timeSkip = 115;//Get this from database
+  var intro = 34; //call from database depending on tvName
+  var toThisPoint = timeSkip + intro;
+  var tvLong = $("span").text();
+  var locNum = tvLong.search("Season");
+  var tvName = tvLong.slice(0, locNum);
+  var tvSeason = tvLong.slice(locNum + 7, locNum + 8);
+  var tvEpisode = tvLong.slice(locNum + 14, locNum + 15);
   currentWindow.toString();
   if(currentWindow==="https://www.netflix.com/browse"){
     console.log("Browsing");
-  }else {
-    watch();
-  }
-}, 500);
-*/
-var timeSkip = 115;//Get this from database
-var intro = 34; //call from database depending on tvName
-var toThisPoint = timeSkip + intro;
-var tvLong = $("span").text();
-var locNum = tvLong.search("Season");
-var tvName = tvLong.slice(0, locNum);
-var tvSeason = tvLong.slice(locNum + 7, locNum + 8);
-var tvEpisode = tvLong.slice(locNum + 14, locNum + 15);
-$(document).ready(function () {
+  } else {
     localStorage.clear();
-    time = setInterval(function () {
-        var skip = $("video").get(0).currentTime;
-        var time = parseInt(skip);
-        if (time >= timeSkip && time < toThisPoint - 1) {
-            if (!localStorage.getItem("skipped")) {
-                window.open(window.location.href + "&t=" + toThisPoint, "_self");
-                localStorage.setItem("skipped", tvName + "." + tvSeason + "." + tvEpisode);
-            }
+    var skip = $("video").get(0).currentTime;
+    var time = parseInt(skip);
+    if (time >= timeSkip && time < toThisPoint - 1) {
+      if (!localStorage.getItem("skipped")) {
+          window.open(window.location.href + "&t=" + toThisPoint, "_self");
+          localStorage.setItem("skipped", tvName + "." + tvSeason + "." + tvEpisode);
+          }
         }
-
+}
         console.log(time);
 
     }, 500);
