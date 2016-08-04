a68f81bd6ad451130614eec8f706fb69fe64bc55
nick <capitals56@gmail.com>
2016-08-04 15:29:32 -0400

    Minor changes

--- content.js
+++ content.js
@@ -1,34 +1,30 @@
 $(document).ready(function () {
-
-  chrome.windows.create({'url': 'popup.html', 'type': 'popup'}, function(window) {
-   });
-
-  time = setInterval(function() {
-      var currentWindow = window.location.href;
-      var timeSkip = 115;//Get this from database
-      var intro = 34; //call from database depending on tvName
-      var toThisPoint = timeSkip + intro;
-      var tvLong = $("span").text();
-      var locNum = tvLong.search("Season");
-      var tvName = tvLong.slice(0, locNum);
-      var tvSeason = tvLong.slice(locNum + 7, locNum + 8);
-      var tvEpisode = tvLong.slice(locNum + 14, locNum + 15);
-      currentWindow.toString();
-      if (currentWindow === "https://www.netflix.com/browse") {
-          console.log("Browsing");
-      } else {
-          localStorage.clear();
-          var skip = $("video").get(0).currentTime;
-          var time = parseInt(skip);
-          if (time >= timeSkip && time < toThisPoint - 1) {
-              if (!localStorage.getItem("skipped")) {
-                  window.open(window.location.href + "&t=" + toThisPoint, "_self");
-                  localStorage.setItem("skipped", tvName + "." + tvSeason + "." + tvEpisode);
-              }
+  time = setInterval(function(){
+    var currentWindow = window.location.href;
+    var timeSkip = 115;//Get this from database
+    var intro = 34; //call from database depending on tvName
+    var toThisPoint = timeSkip + intro;
+    var tvLong = $("span").text();
+    var locNum = tvLong.search("Season");
+    var tvName = tvLong.slice(0, locNum);
+    var tvSeason = tvLong.slice(locNum + 7, locNum + 8);
+    var tvEpisode = tvLong.slice(locNum + 14, locNum + 15);
+    currentWindow.toString();
+    if(currentWindow==="https://www.netflix.com/browse"){
+      console.log("Browsing");
+    } else {
+      localStorage.clear();
+      var skip = $("video").get(0).currentTime;
+      var time = parseInt(skip);
+      if (time >= timeSkip && time < toThisPoint - 1) {
+        if (!localStorage.getItem("skipped")) {
+            window.open(window.location.href + "&t=" + toThisPoint, "_self");
+            localStorage.setItem("skipped", tvName + "." + tvSeason + "." + tvEpisode);
+            }
           }
-      }
-      console.log(time);
+  }
+          console.log(time);
 
-  }, 500);
+      }, 500);
 
 });
