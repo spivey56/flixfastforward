78048714273b215f4b972b96c279c2f02767c882
Drew Gallagher <drewg2009@gmail.com>
2016-08-16 22:57:00 -0400

    added CORS middleware/added headers for allow control allow origin to routes file

--- flixFastForwardBackend/app/Http/routes.php
+++ flixFastForwardBackend/app/Http/routes.php
@@ -15,9 +15,11 @@ Route::get('/', function () {
     return view('admin/intros/index');
 });
 
-Route::get("admin/intros/times/{title}",function($title){
+Route::get("admin/intros/times/{title}", ['middleware' => 'cors', function($title){
+    header('Access-Control-Allow-Origin: *');
+    header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
     $intros=new \App\Http\Controllers\Admin\IntrosController();
     echo $intros->getStartAndSkipTimes($title);
-});
+}]);
 
 Route::resource('admin/intros', 'Admin\\IntrosController');
