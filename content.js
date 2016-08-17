04801abc4f33328619d51a65e8fa3381ad5412f6
Drew Gallagher <drewg2009@gmail.com>
2016-08-17 16:46:12 -0400

    added parse of time variables

--- content.js
+++ content.js
@@ -1,92 +1,101 @@
+var dataObject = null;
+
 $(document).ready(function () {
   var goneIn = false;
-  time = setInterval(function(){
+  var timeSkip = "";
+  var intro = "";
+  time = setInterval(function () {
     var currentWindow = window.location.href;
-    var timeSkip = localStorage.getItem("skipTime");//Get this from database
-    var intro = localStorage.getItem("skipDuration"); //call from database depending on tvName
+    if (dataObject != null) {
+      //console.log(dataObject)
+      timeSkip = parseInt(dataObject.start_time);
+      intro = parseInt(dataObject.duration);
+    }
+    else {
+      console.log("NULL DATAOBJ: " + dataObject);
+    }
     var toThisPoint = timeSkip + intro;
     var tvLong = $("span").text();
     var locNum = tvLong.search("Season");
     var tvName = tvLong.slice(0, locNum);
-    if(tvLong.slice(locNum + 8, locNum + 9)===":"){
+    if (tvLong.slice(locNum + 8, locNum + 9) === ":") {
       var tvSeason = tvLong.slice(locNum + 7, locNum + 8);
     } else {
       var tvSeason = tvLong.slice(locNum + 7, locNum + 9);
     }
     var locEp = tvLong.search("Ep.");
-    var isInt = tvLong.slice(locEp + 5, locEp+6);
+    var isInt = tvLong.slice(locEp + 5, locEp + 6);
 
     //if isInt is not a number
-    if(isNaN(isInt)){
-      var tvEpisode = tvLong.slice(locEp+4,locEp+5);
-    } else{ //if it is a number
-      var tvEpisode = tvLong.slice(locEp+4,locEp+6);
+    if (isNaN(isInt)) {
+      var tvEpisode = tvLong.slice(locEp + 4, locEp + 5);
+    } else { //if it is a number
+      var tvEpisode = tvLong.slice(locEp + 4, locEp + 6);
     }
 
     var title = tvName + "." + tvSeason + "." + tvEpisode;
-    if(title!=".." && goneIn==false){
+    if (title != ".." && goneIn == false) {
       goneIn = true;
       ajaxCall(title);
 
     }
-    if(currentWindow==="https://www.netflix.com/browse"){
+    if (currentWindow === "https://www.netflix.com/browse") {
       console.log("Browsing");
-    } else {
-      localStorage.clear();
+    } 
+    else {
       var skip = $("video").get(0).currentTime;
       var time = parseInt(skip);
+
       if (time >= timeSkip && time < toThisPoint - 1) {
-      if (!localStorage.getItem("skipped")) {
-  			var shouldLoad = currentWindow.search("&t=");
-  			if(shouldLoad>0){
-  				window.open(window.location.href, "_self");
-  			} else {
-  				window.open(window.location.href + "&t=" + toThisPoint, "_self");
-  			}
-			  document.getElementById("loading").style.display = "none"
-            localStorage.setItem("skipped", tvName + "." + tvSeason + "." + tvEpisode);
-            }
-          }
-  }
-      }, 500);
+        var shouldLoad = currentWindow.search("&t=");
+        if (shouldLoad > 0) {
+          window.open(window.location.href, "_self");
+        } else {
+          window.open(window.location.href + "&t=" + toThisPoint, "_self");
+        }
+      }
+    }
+
+  }, 500);
 
 });
 function ajaxCall(title) {
   console.log("MADE IT");
-    //-----------------------------------------------------------------------
-    // 2) Send a http request with AJAX http://api.jquery.com/jQuery.ajax/
-    //-----------------------------------------------------------------------
-    $.ajax({
+  //-----------------------------------------------------------------------
+  // 2) Send a http request with AJAX http://api.jquery.com/jQuery.ajax/
+  //-----------------------------------------------------------------------
+  $.ajax({
     //url: 'https://localhost:8000/admin/intros/times/'+title,
-      url: 'https://phplaravel-19273-43928-140812.cloudwaysapps.com/admin/intros/times/'+title,                  //the script to call to get data                      //you can insert url argumnets here to pass to api.php
-      type: "GET",                                 //for example "id=5&parent=6"
-      success: function(data)          //on recieve of reply
-      {
-        console.log("Success!");
-        var dataObject = $.parseJSON(data);
-        localStorage.setItem("skipTime", dataObject.start_time);
-        localStorage.setItem("skipDuration", dataObject.duration);
-        console.log(localStorage.getItem("skipDuration"));
-        console.log(localStorage.getItem("skipTime"));
+    url: 'https://phplaravel-19273-43928-140812.cloudwaysapps.com/admin/intros/times/' + title,                  //the script to call to get data                      //you can insert url argumnets here to pass to api.php
+    type: "GET",                                 //for example "id=5&parent=6"
+    success: function (data)          //on recieve of reply
+    {
+      console.log("Success!");
+      dataObject = $.parseJSON(data);
 
-      },
-// start snippet
-error: function(XMLHttpRequest, textStatus, errorThrown) {
-    console.log("ERROR has occurred.");
-        if (XMLHttpRequest.readyState == 4) {
-            // HTTP error (can be checked by XMLHttpRequest.status and XMLHttpRequest.statusText)
+      //localStorage.setItem("skipTime", dataObject.start_time);
+      //localStorage.setItem("skipDuration", dataObject.duration);
+      //console.log(localStorage.getItem("skipDuration"));
+      //console.log(localStorage.getItem("skipTime"));
 
-            console.log("HTTP ERROR " + textStatus);
-        }
-        else if (XMLHttpRequest.readyState == 0) {
-            // Network error (i.e. connection refused, access denied due to CORS, etc.)
-            console.log("CORS Error "+ textStatus);
-        }
-        else {
-            // something weird is happening
-            console.log("Something weird is happening!");
-        }
+    },
+    // start snippet
+    error: function (XMLHttpRequest, textStatus, errorThrown) {
+      console.log("ERROR has occurred.");
+      if (XMLHttpRequest.readyState == 4) {
+        // HTTP error (can be checked by XMLHttpRequest.status and XMLHttpRequest.statusText)
+
+        console.log("HTTP ERROR " + textStatus);
+      }
+      else if (XMLHttpRequest.readyState == 0) {
+        // Network error (i.e. connection refused, access denied due to CORS, etc.)
+        console.log("CORS Error " + textStatus);
+      }
+      else {
+        // something weird is happening
+        console.log("Something weird is happening!");
+      }
     }
-//end snippet
-    });
-  }
+    //end snippet
+  });
+}
