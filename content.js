1e7613af5bc12c1c048d5f68656b47ee12b5a735
Drew Gallagher <drewg2009@gmail.com>
2016-08-18 00:06:12 -0400

    cleaned up/simplifed code. added session storage to reduce # of times of ajax request to once per session; uses timeskip and intro from first call stored in storage to skip on reload. skip now works everytime when backed up after already reloading; I had to increase the difference between toThisPoint because that comparison was spotty when you only subtract by 1

--- content.js
+++ content.js
@@ -1,104 +1,95 @@
-var dataObject = null;
-var goneIn = false;
-var win = window.location.href;
+var dataObject;
+var timeSkip;
+var intro;
+
 $(document).ready(function () {
-  var timeSkip = "";
-  var intro = "";
-  time = setInterval(function () {
-    var currentWindow = window.location.href;
-    if (dataObject != null) {
-      //console.log(dataObject)
-      timeSkip = parseInt(dataObject.start_time);
-      intro = parseInt(dataObject.duration);
-    }
-    else {
-      console.log("NULL DATAOBJ: " + dataObject);
-    }
-    var toThisPoint = timeSkip + intro;
-    var tvLong = $("span").text();
-    var locNum = tvLong.search("Season");
-    var tvName = tvLong.slice(0, locNum);
-    if (tvLong.slice(locNum + 8, locNum + 9) === ":") {
-      var tvSeason = tvLong.slice(locNum + 7, locNum + 8);
-    } else {
-      var tvSeason = tvLong.slice(locNum + 7, locNum + 9);
-    }
-    var locEp = tvLong.search("Ep.");
-    var isInt = tvLong.slice(locEp + 5, locEp + 6);
+    dataObject = null;
 
-    //if isInt is not a number
-    if (isNaN(isInt)) {
-      var tvEpisode = tvLong.slice(locEp + 4, locEp + 5);
-    } else { //if it is a number
-      var tvEpisode = tvLong.slice(locEp + 4, locEp + 6);
-    }
-    var title = tvName + "." + tvSeason + "." + tvEpisode;
+    time = setInterval(function () {
 
-    if ((title !== ".." && !goneIn)||(win!==currentWindow)) {
-      dataObject = null;
-      ajaxCall(title);
-    }
-
-    console
-    if (currentWindow === "https://www.netflix.com/browse") {
-      console.log("Browsing");
-    }
-    else {
-      var skip = $("video").get(0).currentTime;
-      var time = parseInt(skip);
-
-      if (time >= timeSkip && time < toThisPoint - 1) {
-        var shouldLoad = currentWindow.search("&t=");
-        if (shouldLoad > 0) {
-          window.open(window.location.href, "_self");
+        var tvLong = $("span").text();
+        var locNum = tvLong.search("Season");
+        var tvName = tvLong.slice(0, locNum);
+        if (tvLong.slice(locNum + 8, locNum + 9) === ":") {
+            var tvSeason = tvLong.slice(locNum + 7, locNum + 8);
         } else {
-          window.open(window.location.href + "&t=" + toThisPoint, "_self");
+            var tvSeason = tvLong.slice(locNum + 7, locNum + 9);
         }
-      }
-    }
+        var locEp = tvLong.search("Ep.");
+        var isInt = tvLong.slice(locEp + 5, locEp + 6);
 
-  }, 500);
+        //if isInt is not a number
+        if (isNaN(isInt)) {
+            var tvEpisode = tvLong.slice(locEp + 4, locEp + 5);
+        } else { //if it is a number
+            var tvEpisode = tvLong.slice(locEp + 4, locEp + 6);
+        }
+        var title = tvName + "." + tvSeason + "." + tvEpisode;
+
+        if (title != "..") {
+
+            if (sessionStorage.getItem("timeSkip") && sessionStorage.getItem("intro")) {
+                timeSkip = parseInt(sessionStorage.getItem("timeSkip"));
+                intro = parseInt(sessionStorage.getItem("intro"));
+            }
+            else {
+
+                if (dataObject == null) {
+                    ajaxCall(title);
+                }
+                else {
+                    timeSkip = parseInt(dataObject.start_time);
+                    intro = parseInt(dataObject.duration);
+                }
+            }
+
+        }
+
+        if (window.location.href != "https://www.netflix.com/browse" && window.location.href.search("watch") > 0) {
+
+            var skip = $("video").get(0).currentTime;
+            var currentTime = parseInt(skip);
+
+            if (currentTime >= timeSkip && currentTime < (timeSkip+intro) - 4) {
+                window.open(window.location.href + "&t=" + (timeSkip+intro), "_self");
+            }
+        }
+
+    }, 100);
 
 });
 function ajaxCall(title) {
-  console.log("MADE IT");
-  console.log(title);
-  //-----------------------------------------------------------------------
-  // 2) Send a http request with AJAX http://api.jquery.com/jQuery.ajax/
-  //-----------------------------------------------------------------------
-  $.ajax({
-    //url: 'https://localhost:8000/admin/intros/times/'+title,
-    url: 'https://phplaravel-19273-43928-140812.cloudwaysapps.com/admin/intros/times/' + title,                  //the script to call to get data                      //you can insert url argumnets here to pass to api.php
-    type: "GET",                                 //for example "id=5&parent=6"
-    success: function (data)          //on recieve of reply
-    {
-      console.log("Success!");
-      dataObject = $.parseJSON(data);
-      goneIn = true;
-      win = window.location.href;
-      //localStorage.setItem("skipTime", dataObject.start_time);
-      //localStorage.setItem("skipDuration", dataObject.duration);
-      //console.log(localStorage.getItem("skipDuration"));
-      //console.log(localStorage.getItem("skipTime"));
+    console.log(title);
 
-    },
-    // start snippet
-    error: function (XMLHttpRequest, textStatus, errorThrown) {
-      console.log("ERROR has occurred.");
-      if (XMLHttpRequest.readyState == 4) {
-        // HTTP error (can be checked by XMLHttpRequest.status and XMLHttpRequest.statusText)
+    $.ajax({
+        //url: 'https://localhost:8000/admin/intros/times/'+title,
+        url: 'https://phplaravel-19273-43928-140812.cloudwaysapps.com/admin/intros/times/' + title,                  //the script to call to get data                      //you can insert url argumnets here to pass to api.php
+        type: "GET",
+        success: function (data)          //on recieve of reply
+        {
+            console.log("Success!");
+            dataObject = $.parseJSON(data)
+            sessionStorage.setItem("timeSkip", dataObject.start_time);
+            sessionStorage.setItem("intro", dataObject.duration);
 
-        console.log("HTTP ERROR " + textStatus);
-      }
-      else if (XMLHttpRequest.readyState == 0) {
-        // Network error (i.e. connection refused, access denied due to CORS, etc.)
-        console.log("CORS Error " + textStatus);
-      }
-      else {
-        // something weird is happening
-        console.log("Something weird is happening!");
-      }
-    }
-    //end snippet
-  });
+        },
+        // start snippet
+        error: function (XMLHttpRequest, textStatus, errorThrown) {
+            console.log("ERROR has occurred.");
+            if (XMLHttpRequest.readyState == 4) {
+                // HTTP error (can be checked by XMLHttpRequest.status and XMLHttpRequest.statusText)
+
+                console.log("HTTP ERROR " + textStatus);
+            }
+            else if (XMLHttpRequest.readyState == 0) {
+                // Network error (i.e. connection refused, access denied due to CORS, etc.)
+                console.log("CORS Error " + textStatus);
+            }
+            else {
+                // something weird is happening
+                console.log("Something weird is happening!");
+            }
+        }
+        //end snippet
+    });
 }
